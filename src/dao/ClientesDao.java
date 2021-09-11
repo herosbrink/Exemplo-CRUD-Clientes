b3eb5a53c6c092b5362635325360a3dcc9e75b0e
vmari <vmarins.dev@gmail.com>
2021-09-11 03:59:40 -0300

    Adicionada Tabela e método para listar clientes.

--- src/dao/ClientesDao.java
+++ src/dao/ClientesDao.java
@@ -3,7 +3,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javabeans.Clientes;
 import jdbc.ConnectionFactory;
 
@@ -46,5 +49,41 @@ public class ClientesDao {
             throw new  RuntimeException(e);
         }
     }
+    
+    // Método para listar os clientes na Jtable
+    public List<Clientes> listarClientes(){
+        try {
+            
+           // 1º Criar o vetor que vai armazenar os registros do BD
+            List<Clientes> lista = new ArrayList<Clientes>();
+            
+            // 2º Criar o comando SQL
+            String cmdSql = "select * from clientes";
+            
+            //  Organizar o comando para execução
+            PreparedStatement stmt = conecta.prepareStatement(cmdSql);
+            
+            // 3º Guardar o resultado do "select" dentro do objeto "rs" que é do tipo ResultSet
+            ResultSet rs = stmt.executeQuery();
+            
+            // 4º Enquanto houver registros (resultado do select) guarde na lista
+            while(rs.next()){
+                Clientes cli = new Clientes();
+                cli.setIdcliente(rs.getInt("idcliente"));
+                cli.setNome(rs.getString("nome"));
+                cli.setEmail(rs.getString("email"));
+                cli.setTelefone(rs.getString("telefone"));
+                
+                lista.add(cli);
+            }
+            return lista;
+            
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        
+        
+    }
+    
     
 }
